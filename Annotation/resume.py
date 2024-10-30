82067be93c2e794dfa170f694ac9c89998b11b96
unknown <henriquebonfimdev@gmail.com>
2024-10-30 03:33:04 -0300

    update with type convertation, variables, ellipsis, f-string and controls decimal places

--- Annotation/resume.py
+++ Annotation/resume.py
@@ -7,11 +7,41 @@ print("Any words")
 Multi-line comment 
 """
 
-
-# 
+# named argument (sep)
 print(10, 12, sep=" ")
 
 # \n (Return) and \n (breakLine)
 print(10, 12, sep=" ", end='\n')
 
-print(4)
+# Python is a strongly typed programming language 
+# Dynamic typing: Python already knows the type of information we are passing..
+
+#Data Type
+
+#String: text type, inside ' x '
+print('any text')
+
+#int: any number without punctuation
+print(48)
+
+#float: any number without punctuation (.)
+print(12.9, -8.1) 
+
+#boolean: false or true
+print(False, True)
+
+#typeof: show the type value inside. Its a class type (call able)
+print(type(12.))
+
+#type convertion: int(), float(), str()
+print(int("3")+1)
+
+#Create Variable
+var_test = "any value"
+
+#ellipsis
+var = ... #test ellipsis
+
+#string formatting: f'any text{}'
+var = f'{var2} test'
+
