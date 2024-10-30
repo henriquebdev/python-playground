f9acf41f8d6b641eb112a9de4f19a76117c1e225
unknown <henriquebonfimdev@gmail.com>
2024-10-30 01:20:53 -0300

    Create the annotation folder and making the first annotation

---
+++ Annotation/resume.py
@@ -0,0 +1,17 @@
+#do Comment
+
+#Print words in console
+print("Any words")
+
+"""
+Multi-line comment 
+"""
+
+
+# 
+print(10, 12, sep=" ")
+
+# \n (Return) and \n (breakLine)
+print(10, 12, sep=" ", end='\n')
+
+print(4)
