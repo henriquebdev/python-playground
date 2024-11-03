2b3f34b5186b5243e33f937f3fc4e6aa402c58c5
unknown <henriquebonfimdev@gmail.com>
2024-11-03 20:56:37 -0300

    Creating_guess_the_word exercise

---
+++ ex2_guess_the_word.py
@@ -0,0 +1,16 @@
+'''
+
+ Summary: The User needs to discover the hidden word. To do this, he can try as many times as necessary.
+ He will only need to type to one word at time.
+
+ 1º condition: If the type word is correct, the system will show at console the length of the secrect
+ word using stars(*) for the letters that yet not discovery and will show the correct position of the
+word that the user got right.
+
+ 2º condition: If the word is wrong, the system will show at the console just de number of the length
+ using starts. And then it will ask the user for another option.
+
+ Obs: don't forgget the usually validations
+
+ '''
+
