d5af92df65a2f287adf8e9514ce2fa586355ea57
unknown <henriquebonfimdev@gmail.com>
2024-11-08 23:59:18 -0300

    Create exercise market list

---
+++ ex3_market_list.py
@@ -0,0 +1,14 @@
+'''
+
+ Summary: Create a market list where the user must have the possibility to (insert, delete(using index)
+ and list)
+
+ Obs: don't forgget the usually validations
+
+ '''
+
+loop_controller = False
+
+
+while loop_controller == False:
+    chosen_option = input("Select any option\n[i]nset [d]elete [l]ist: ")
