b5b8c1531d4eae2fd49bc0ca985a3dc6eafa272b
unknown <henriquebonfimdev@gmail.com>
2024-11-12 07:57:37 -0300

    Dictionaries methods:len, keys, values, items, setdefault, copy, pop, pop item and update

--- Module_2_intermediate/class_4.py
+++ Module_2_intermediate/class_4.py
@@ -8,6 +8,50 @@ people = {
 
 print(people['name'])
 
+#Useful methods
 
+#len
+print(len(people))
 
+#keys
+print(people.keys())
 
+#Values
+print(people.values())
+
+#items
+print(people.items())
+
+#setdefault
+people = {
+    'name': 'Luiz',
+    'age': '28',
+    #'city': 'sp'
+}
+people.setdefault('city')
+print(people['city'])
+
+#copy
+second_people = people.copy()
+print('Second people print: ', second_people['name'])
+
+#get
+
+#pop
+people.pop('name')
+
+print("pop", people)
+
+#popitem
+
+people.popitem()
+
+print("pop", people)
+
+#update
+
+people.update(
+   { 'color':'blue'}
+)
+
+print(people)
