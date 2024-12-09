2c883cf09858c09d8d8996215cf9ba6d60fa4791
unknown <henriquebonfimdev@gmail.com>
2024-12-09 12:45:55 -0300

    Singleton, module and package

---
+++ class_11.py
@@ -0,0 +1,10 @@
+'''
+Singleton: Is a design pattern that ensures that a class has only a single instance during program
+execution and provides a global point of access to that instance
+
+Module: It's a python file  
+Package: It's a folder that contains python files
+
+
+'''
+
